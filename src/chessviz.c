030ac8dae84b5fac3d468eb167ab0e2464465606
kitosek <dolggik@gmail.com>
2018-04-23 15:07:07 +0700

    Split program to 3 files and modiffy Makefile

---
+++ src/chessviz.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "form.h"
+int main ()
+{
+	//int i,j;
+	char A[10][10];
+	FirstDoska(A);
+	PrintDoska(A);
+	board(A);
+	PrintDoska(A);
+	return 0;
+}
---
+++ src/form.h
@@ -0,0 +1,6 @@
+#ifndef _chessviz_form_h
+#define _chessviz_form_h
+void PrintDoska(char A[10][10]);
+void FirstDoska(char A[10][10]);
+void board(char A[10][10]);
+#endif
